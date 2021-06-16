daeb2f4600dcd6fdd747a822ad3f33da99fc26b1
TatoBig <tatobig@gmail.com>
2021-06-16 11:08:55 -0600

    Menu inicial

---
+++ components/visual/Buttons.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Button, makeStyles } from '@material-ui/core/'
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  }
+}))
+
+export const OptionButton = (props) => {
+  const { text } = props
+  const classes = useStyles()
+  return (
+    <React.Fragment>
+      <Button className={classes.button} fullWidth>
+        {text}
+      </Button>
+    </React.Fragment>
+  )
+}
